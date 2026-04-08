1f6ec62bcdff0f3ad2ef1bc87fd14b4445ab0ef5
Okezie <okezievictor45@gmail.com>
2026-04-08 14:11:25 +0100

    Added auto date

--- applications/models.py
+++ applications/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Application(models.Model):
     company = models.CharField(max_length = 60)
     role = models.CharField(max_length=200)
-    date_applied = models.DateField()
+    date_applied = models.DateField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=[("Applied", "Applied"), ("Interviewing", "Interviewing"), ("Rejected", "Rejected"), ("Offer", "Offer")])
 
 
